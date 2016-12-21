01f3237388f9a7d64c4a9d3200112190ff4d1d0f
Shreya4net <pujakumari.civil@gmail.com>
2016-12-21 11:33:10 +0530

    java session1 assignment 2
    session1 assignment2

---
+++ Numbers.java
@@ -0,0 +1,25 @@
+package no;
+
+public class Numbers {
+	public static void main(String[] args)
+	{
+		int a=20;
+		int b=10;
+		int c=a-- -  --a;
+		int d=a--;
+		int e=a>>2;
+		int f=a&b;
+		System.out.println("c="+c);
+		System.out.println("d="+d);
+		System.out.println("e="+e);
+		System.out.println("f="+f);
+	}
+		
+				
+	}
+	
+	
+	
+	
+
+
